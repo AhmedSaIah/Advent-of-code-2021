16cb822c591cdd9a2e9179b2948cf9b77b831a72
EhabSalah <ehab.salah020@gmail.com>
2021-12-07 13:11:16 +0200

    functionality update

--- src/main/java/Contest/Day1.java
+++ src/main/java/Contest/Day1.java
@@ -2,67 +2,43 @@ package Contest;
 //--- Day 1 " Sonar Sweep " ---
 // https://adventofcode.com/2021/day/1
 
-import java.util.*;
 import java.util.ArrayList;
-
 import java.io.*;
+import java.util.Scanner;
 
 public class Day1 {
     public static void main(String[] args) throws Exception {
-        System.out.println(partOneIncreasmentValue(readFile("src/main/resources/input.txt")));
-        System.out.println(partTwoIncreasmentValue(readFile("src/main/resources/input.txt")));
-    }
-
-    public static ArrayList<Integer> readFile(String filePath) throws Exception {
-        File file = new File(filePath);
-        Scanner sc = new Scanner(file);
-
+        File file = new File("src/main/resources/input.txt");
+        Scanner myReader = new Scanner(file);
         ArrayList<Integer> numbers = new ArrayList<>();
-        while (sc.hasNextLine()) {
-            numbers.add(Integer.parseInt(sc.nextLine()));
-
+        while (myReader.hasNextLine()) {
+            numbers.add(Integer.parseInt(myReader.nextLine()));
         }
-        return numbers;
+
+        int part1 = day1Solve(numbers, 1);
+        int part2 = day1Solve(numbers, 3);
+        //part 1
+        System.out.println(part1);
+        //part 2
+        System.out.println(part2);
+    }
 
 /*
         Checking the record of each line in the file by using this loop
        for (int i : numbers) {
        System.out.println(i);
        }
-
 */
-    }
 
-    public static int partOneIncreasmentValue(ArrayList<Integer> arr) {
+    public static int day1Solve(ArrayList<Integer> input, int count) {
 
-        int previousValue = arr.get(0);
+        //int previousValue = input.get(0);
         int increaseTimes = 0;
-
-        for (int i = 1; i <= arr.size() - 1; i++) {
-            if (arr.get(i) > previousValue) {
-                increaseTimes++;
-                previousValue = arr.get(i);
-            } else
-                previousValue = arr.get(i);
-
+        for (int index = 0; index < input.size() - count; index++) {
+            increaseTimes =
+                    increaseTimes + (input.get(index + count) > input.get(index) ? 1 : 0);
         }
-
         return increaseTimes;
     }
 
-
-    public static int partTwoIncreasmentValue(ArrayList<Integer> arr){
-        int i=0;
-        int increasment = 0;
-        int sumOfPreviousThreeValues = arr.get(i)+arr.get(i+1)+arr.get(i+2);
-        int sumOfNewThreeValues= arr.get(i+3)+arr.get(i+4)+arr.get(i+5);
-        for(i=0; i<arr.size()-2; i++){
-            if(sumOfPreviousThreeValues < sumOfNewThreeValues){
-                increasment++;
-                i++;
-            }
-
-        }
-        return increasment;
-    }
 }
